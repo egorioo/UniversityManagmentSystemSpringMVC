3bdf18f10aebf1df87cc0731cbac839c9b3fe1ed
egorioo <egorrudenko2002@gmail.com>
2022-01-18 14:18:30 +0200

    added crud operations for faculties and groups

--- src/main/java/spring/models/Group.java
+++ src/main/java/spring/models/Group.java
@@ -4,6 +4,15 @@ public class Group {
     private int id;
     private String groupCode;
     private String fullName;
+    private int facultyId;
+
+    public int getFacultyId() {
+        return facultyId;
+    }
+
+    public void setFacultyId(int facultyId) {
+        this.facultyId = facultyId;
+    }
 
     public int getId() {
         return id;
@@ -27,5 +36,15 @@ public class Group {
 
     public void setFullName(String fullName) {
         this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "id=" + id +
+                ", groupCode='" + groupCode + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", facultyId=" + facultyId +
+                '}';
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,15 @@
 <html>
 <body>
-<a href="/students">redirect</a>
+<form action="/students">
+    <input type="submit" value="Students">
+</form>
+
+<form action="/faculties">
+    <input type="submit" value="Faculties">
+</form>
+
+<form action="/groups">
+    <input type="submit" value="Groups">
+</form>
 </body>
 </html>
